cd5fc0b46399c1f01492d1ebd416736017cd0433
Carlo-Falanga <carlofalanga7@gmail.com>
2026-04-07 11:17:53 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+const postsRouter = require("./routes/posts");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static assets from ../public
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+
+// Starts the listener
+app.listen(PORT, () => {
+  console.log(`Server in ascolto su http://localhost:${PORT}`);
+});
+
+
+// Server entry point
+app.get("/", (req, res) => {
+  res.type("text").send("Server del mio blog");
+});
+
+
+// Posts CRUD routes
+// Use posts router for /posts routes
+app.use("/posts", postsRouter);
