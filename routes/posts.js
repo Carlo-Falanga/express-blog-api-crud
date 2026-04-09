d8d44f6805a74c3770eeee952ba755091b6c4a12
Carlo-Falanga <carlofalanga7@gmail.com>
2026-04-09 12:58:06 +0200

    remove unused middleware import from posts route

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const postController = require("../controllers/postController");
-const checkPostExist = require("../middlewares/serverError")
 
 // CRUD
 
